ffff87c7da55ab1c377cb363fdc502f3fbaccc2e
Kamil Doległo <kamil.doleglo@gmail.com>
2018-12-05 10:13:19 +0100

    Fix diagram and classes

--- src/main/java/application/GameApplication.java
+++ src/main/java/application/GameApplication.java
@@ -1,12 +1,14 @@
 package application;
 
 import controller.Controller;
+import menu.Menu;
 
 public class GameApplication {
     private int windowSizeX;
     private int windowsizeY;
     private Controller controller;
     private GameState gameState;
+    private Menu menu;
 
     public void run(){}
 }
